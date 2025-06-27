72c7e484b6d4054e16c8d34a409541a64b1f4cde
rararay99 <rarara_y@yahoo.co.jp>
2025-06-28 02:38:14 +0900

    Create q1_select.py

---
+++ q1_select.py
@@ -0,0 +1,15 @@
+# 問題：次のうち、「Hello, Python!」と表示するコードはどれか？
+
+# A
+# print("Hello, Python!")
+
+# B
+# echo("Hello, Python!")
+
+# C
+# printf("Hello, Python!")
+
+# D
+# console.log("Hello, Python!")
+
+# 回答：A
